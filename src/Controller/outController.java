0237747a1f8d252069a0c8725ad08a117c3c7fcf
Insoo Kim <42515875+Insookim0702@users.noreply.github.com>
2018-09-17 14:35:21 +0900

    version 0.2 -> 퇴실을 담당하는 Controller
    *수정사항: 
    1. 퇴실과 관련된 반복되는 코드를 줄임. 
              - switch문을 사용해서 out1,out2,out3,out4,out5,out6,out7,out8,out9 함수를 다 없앨 수 있었음. 
    2. dao.outregister1() -> dao.outdao(침대번호) 
              - outDAO에 있는 outregister1~9까지 함수를 다 지우고 outdao로 통일시킴.

--- src/Controller/outController.java
+++ src/Controller/outController.java
@@ -17,116 +17,32 @@ public class outController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String command =request.getParameter("command");
-			if(command.equals("out1")) {
-				out1(request, response);
-			}else if(command.equals("out2")) {
-				out2(request, response);
-			}else if(command.equals("out3")) {
-				out3(request, response);
-			}else if(command.equals("out4")) {
-				out4(request, response);
-			}else if(command.equals("out5")) {
-				out5(request, response);
-			}else if(command.equals("out6")) {
-				out6(request, response);
-			}else if(command.equals("out7")) {
-				out7(request, response);
-			}else if(command.equals("out8")) {
-				out8(request, response);
-			}else if(command.equals("out9")) {
-				out9(request, response);
+			out(request, response);
+	}
+	
+	private void out(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String num;
+		try {
+			outDAO dao =new outDAO();
+			num = request.getParameter("bednum");
+			switch(num) {
+			
+			case "1":	dao.outdao("1");	break;
+			case "2":	dao.outdao("2");	break;
+			case "3":	dao.outdao("3");	break;
+			case "4":	dao.outdao("4");	break;
+			case "5":	dao.outdao("5");	break;
+			case "6":	dao.outdao("6");	break;
+			case "7":	dao.outdao("7");	break;
+			case "8":	dao.outdao("8");	break;
+			case "9":	dao.outdao("9");	break;
+			default:	System.out.println("형식이 올바르지 않습니다.");
 			}
-	}
-	private void out1(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister1();
+			
 			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
 		}catch(Exception e) {
 			e.printStackTrace();
 			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
 		}
 	}
-	private void out2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister2();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out3(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister3();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-			request.getRequestDispatcher("jsp/errorView.jsp").forward(request, response);
-		}
-	}
-	private void out4(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister4();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out5(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister5();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out6(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister6();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out7(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister7();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out8(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister8();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-	private void out9(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			outDAO dao =new outDAO();
-			dao.outregister9();
-			request.getRequestDispatcher("/registerController?command=reload").forward(request, response);
-		}catch(Exception e) {
-			e.printStackTrace();
-			request.setAttribute("errorMsg","죄송합니다. 잠시 후에 재 실행해 주세요.");
-		}
-	}
-
 }
