294305c47a8c321fb4cd9d3108b50e4d45f80c7a
Insoo Kim <42515875+Insookim0702@users.noreply.github.com>
2018-09-17 14:58:01 +0900

    version0.2 -> 퇴실 상태로 DB에 update하는 DAO
    *수정 사항
     1. outregister1() ~  outregister9() 를 없애고 outdao(String no) 하나로 만듬.
        - ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='"+ no +"'");
       -  ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='"+no+"'");

--- src/model/outDAO.java
+++ src/model/outDAO.java
@@ -8,7 +8,28 @@ import java.util.Calendar;
 import util.DBUtil;
 
 public class outDAO {
-	
+	/*
+	 * *퇴실하기*
+	 */
+	public void outdao(String no) {
+		Connection con =null;
+		PreparedStatement ps =null;
+		ResultSet rset =null;
+		
+		try {
+			con = DBUtil.getConnection();
+			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='"+ no +"'");
+			ps.executeUpdate();
+			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='"+no+"'");
+			ps.executeUpdate();
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("DB에 접근하지 못했습니다.");
+			
+		}
+	}
+
 	/*===================년/월/일/시/분/초 반환하는 클래스===============================*/
 	Calendar cal = Calendar.getInstance();
 	public String getYear() {
@@ -30,178 +51,6 @@ public class outDAO {
 	public int getSecond() {
 		return cal.get(Calendar.SECOND);
 	}
-	
-	
-	
 	String time = getHour()+"시" +getMinute()+" 분"; 
-	/********************퇴실하기************************/
-	public void outregister1() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='1'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='1'");
-			ps.executeUpdate();
-			
-			
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister2() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		//ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='2'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='2'");
-			ps.executeUpdate();
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister3() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		//ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			//ps =con.prepareStatement("UPDATE register SET inout = '퇴실' outtime = "+time+ "WHERE inout ='입실' and bednum ='3'");
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='3'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='3'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister4() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='4'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='4'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister5() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='5'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='5'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister6() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='6'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='6'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister7() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		String time = getHour()+"시" +getMinute()+" 분"; 
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='7'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='7'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister8() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='8'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='8'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
-	/********************퇴실하기************************/
-	public void outregister9() {
-		Connection con =null;
-		PreparedStatement ps =null;
-		ResultSet rset =null;
-		try {
-			con = DBUtil.getConnection();
-			ps = con.prepareStatement("UPDATE register SET outtime='"+time+"' WHERE inout ='입실' and bednum ='9'");
-			ps.executeUpdate();
-			ps =con.prepareStatement("UPDATE register SET inout = '퇴실' WHERE inout ='입실' and bednum ='9'");
-			ps.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("DB에 접근하지 못했습니다.");
-			
-		}
-	}
 	
 }
